41a6675447defcdc4e77b0702c61b6a1f2b34f75
Zafri Anam <zafrianam@gmail.com>
2018-05-30 03:00:56 +0000

    //clswork4 task2

--- script.js
+++ script.js
@@ -7,5 +7,14 @@ console.log(heading.innerText);
 /* Task 2 using plain JavaScript */
 
 
+function addText2() {
+    var task2div = document.getElementById('task2a');
+    var paragraph = document.createElement('p');
+    paragraph.innerText = "Hello World";
+    task2div.appendChild(paragraph);
+}
+
+var textBtn = document.getElementById('textBtn');
+textBtn.addEventListener('click', addText2);
 
 /* Task 4 using jQuery */
