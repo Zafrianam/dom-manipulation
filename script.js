06fdd44419792c60ad33da29ac0721c77663b021
Zafri Anam <zafrianam@gmail.com>
2018-05-30 03:18:57 +0000

    //clswork4 task2c

--- script.js
+++ script.js
@@ -17,4 +17,21 @@ function addText2() {
 var textBtn = document.getElementById('textBtn');
 textBtn.addEventListener('click', addText2);
 
+//Task 2b
+
+function changeBgColor(event) {
+    var body = document.getElementsByTagName('body')[0];
+    if(event.target.innerText=="RED"){
+        body.style.backgroundColor = 'red';
+    }
+    if(event.target.innerText=="GREEN"){
+        body.style.backgroundColor = 'green';
+    }
+}
+
+//Task 2c
+function goToGoogle() {
+    window.location.assign('https://www.google.com/');
+}
+
 /* Task 4 using jQuery */
